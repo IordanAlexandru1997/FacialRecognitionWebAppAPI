22eb55b1ed2cbf7adcf9753893622ddab5a37f95
IordanAlexandru1997 <103210590+IordanAlexandru1997@users.noreply.github.com>
2023-05-04 21:04:57 +0200

    changes to image req2

--- server.js
+++ server.js
@@ -29,10 +29,7 @@ const db = knex({
 // const corsOptions = {
 //   origin: "https://facialrecognition-mq12.onrender.com/",
 // };
-app.use(cors({
-  origin: ['http://localhost:3000', 'https://facialrecognition-mq12.onrender.com'],
-  credentials: true,
-}));
+
 const app = express();
 
 app.use(bodyParser.json());
