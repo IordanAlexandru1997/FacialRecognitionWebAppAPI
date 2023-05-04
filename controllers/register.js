b60150fcc24c7a52cb80820c111aec2d0dcf05bd
IordanAlexandru1997 <103210590+IordanAlexandru1997@users.noreply.github.com>
2023-05-04 09:49:33 +0200

    new changes, fixed issues

--- controllers/register.js
+++ controllers/register.js
@@ -1,37 +1,45 @@
-const handleRegister =(req,res,bcrypt, db)=>{
+const handleRegister = (req, res, bcrypt, db) => {
+    const { email, name, password } = req.body;
 
-    const {email,name,password} = req.body;
-    if (!(email && name && password)){
-       return res.status(400).json('incorrect form of submission')
+    console.log('Request body:', req.body); // Add this line to log the request body
 
+    if (!(email && name && password)) {
+        return res.status(400).json('incorrect form of submission');
     }
     const hash = bcrypt.hashSync(password);
 
-    db.transaction(trx=>{
-         trx.insert({
-             hash: hash,
-             email: email
-         })
-         .into('login')
-         .returning('email')
-         .then(loginEmail =>{
-            return trx('users')
-            .returning('*')
+    db.transaction((trx) => {
+        trx
             .insert({
-                email: loginEmail[0].email,
-                name: name,
-                joined: new Date()
-            })    
-            .then(user=>{ 
-                res.json(user[0]);
+                hash: hash,
+                email: email,
             })
-         })
-         .then(trx.commit)
-         .catch(trx.rollback)
-     })
-    .catch(err=>{ res.status(400).json('could not register user')})
-}
+            .into('login')
+            .returning('email')
+            .then((loginEmail) => {
+                console.log('Login email:', loginEmail); // Add this line to log the login email
+                return trx('users')
+                    .returning('*')
+                    .insert({
+                        email: loginEmail[0].email,
+                        name: name,
+                        joined: new Date(),
+                    })
+                    .then((user) => {
+                        res.json(user[0]);
+                    });
+            })
+            .then(trx.commit)
+            .catch((err) => {
+                console.log('Transaction error:', err); // Add this line to log the transaction error
+                trx.rollback;
+            });
+    }).catch((err) => {
+        console.log('Registration error:', err); // Add this line to log the registration error
+        res.status(400).json('could not register user');
+    });
+};
 
 module.exports = {
-    handleRegister: handleRegister
-}
+    handleRegister: handleRegister,
+};
--- server.js
+++ server.js
@@ -14,41 +14,51 @@ const image = require('./controllers/image');
 
 
 const db = knex({
-    client: 'pg',
-    connection: {
-        connectionString: process.env.DATABASE_URL,
-        ssl: {
-          rejectUnauthorized: false
-        }
-    }
-  });
+  client: 'pg',
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
+});
 
+
+
+
+const corsOptions = {
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  allowedHeaders: 'Content-Type,Authorization',
+  optionsSuccessStatus: 200
+};
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(cors())
+app.use(cors(corsOptions));
 
 
 
-app.get('/about', (req,res)=>{
-    res.send("success");
+app.get('/about', (req, res) => {
+  res.send("success");
 })
 
-app.post('/signin',(req,res)=> signin.handleSignin(req,res,bcrypt,db))
+app.post('/signin', (req, res) => signin.handleSignin(req, res, bcrypt, db))
 
-app.post('/register',(req,res) => register.handleRegister(req,res,bcrypt,db))
+app.post('/register', (req, res) => register.handleRegister(req, res, bcrypt, db))
+// app.get('/register', (req, res) => {
+//   // Your logic here, for example:
+//   res.send('This is the register page');
+// });
+app.get('/profile/:id', (req, res) => profile.handleProfile(req, res, bcrypt, db))
 
-app.get('/profile/:id', (req,res)=>profile.handleProfile(req,res,bcrypt,db))
+app.put('/image', (req, res) => image.handleImage(req, res, db))
 
-app.put('/image', (req,res)=>image.handleImage(req,res,db))
-
-app.post('/imageurl', (req,res)=>image.handleApiCall(req,res))
-
-
-
-app.listen(process.env.PORT || 3000,()=>{
-    console.log(`App is running on port ${process.env.PORT}`)
-})
+app.post('/imageurl', (req, res) => image.handleApiCall(req, res))
 
 
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`App is running on port ${process.env.PORT || 3000}`);
+});
