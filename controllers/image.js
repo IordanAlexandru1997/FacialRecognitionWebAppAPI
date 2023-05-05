d9ce31ff7d0fecd32fc5b85cbdd85ed357c2bfc2
IordanAlexandru1997 <103210590+IordanAlexandru1997@users.noreply.github.com>
2023-05-05 11:39:51 +0200

    clarifai api fix

--- controllers/image.js
+++ controllers/image.js
@@ -5,13 +5,42 @@ const app = new Clarifai.App({
 })
 
 const handleApiCall = (req, res) => {
-  app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
-    .then(data => { res.json(data) })
-    .catch(err => {
-      console.log(err);
+  const IMAGE_URL = req.body.input;
+
+  const raw = JSON.stringify({
+    "user_app_id": {
+      "user_id": "clarifai",
+      "app_id": "main"
+    },
+    "inputs": [
+      {
+        "data": {
+          "image": {
+            "url": IMAGE_URL
+          }
+        }
+      }
+    ]
+  });
+
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': 'Key 4d9db0dce81a452483b5f3a387dc4971'
+    },
+    body: raw
+  };
+
+  fetch(`https://api.clarifai.com/v2/models/face-detection/versions/6dc7e46bc9124c5c8824be4822abe105/outputs`, requestOptions)
+    .then(response => response.json())
+    .then(data => res.json(data))
+    .catch(error => {
+      console.log('error', error);
       return res.status(400).json('Unable to work with API');
-    })
-}
+    });
+};
 
 handleImage = (req, res, db) => {
   const { id } = req.body;
