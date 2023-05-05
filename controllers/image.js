40500d34459e1ca855e363ac0a18a2ee1a74a235
IordanAlexandru1997 <103210590+IordanAlexandru1997@users.noreply.github.com>
2023-05-05 10:21:14 +0200

    clarifai api fix

--- controllers/image.js
+++ controllers/image.js
@@ -6,10 +6,7 @@ const app = new Clarifai.App({
 
 const handleApiCall = (req, res) => {
   app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
-    .then(data => {
-      console.log(data);
-      return res.json(data);
-    })
+    .then(data => { res.json(data) })
     .catch(err => res.status(400).json('Unable to work with API'))
 }
 
