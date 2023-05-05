ab9160d6488b10308ca31eafe50ffa3b0318c4d6
IordanAlexandru1997 <103210590+IordanAlexandru1997@users.noreply.github.com>
2023-05-05 09:53:57 +0200

    cors unnecessary tags removed

--- controllers/image.js
+++ controllers/image.js
@@ -1,25 +1,25 @@
 const Clarifai = require('clarifai')
 
 const app = new Clarifai.App({
-    apiKey: '9b2d44293f9647a1a8967db60290b4a5'
-  })
+  apiKey: '6f185842c4104dcca932ce5f402b7bff'
+})
 
-const handleApiCall = (req,res)=>{
-    app.models.predict(Clarifai.FACE_DETECT_MODEL,req.body.input)
-    .then(data=>{res.json(data)})
-    .catch(err=>res.status(400).json('Unable to work with API'))
-}
-  
-handleImage = (req,res,db)=>{
-    const { id } = req.body;
-   db('users').where('id', '=', id)
-  .increment('entries',1)
-  .returning('entries')
-  .then(entries => res.status(200).send(entries[0].entries))
-  .catch(err=>{ res.status(400).json('unable to update entries')})
+const handleApiCall = (req, res) => {
+  app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => { res.json(data) })
+    .catch(err => res.status(400).json('Unable to work with API'))
 }
 
-module.exports ={
-    handleImage,
-    handleApiCall
+handleImage = (req, res, db) => {
+  const { id } = req.body;
+  db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => res.status(200).send(entries[0].entries))
+    .catch(err => { res.status(400).json('unable to update entries') })
+}
+
+module.exports = {
+  handleImage,
+  handleApiCall
 }
