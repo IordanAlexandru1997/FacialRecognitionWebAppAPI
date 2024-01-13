fa572c967a4ef6255e32e0dcf146eee654b11a67
IordanAlexandru1997 <103210590+IordanAlexandru1997@users.noreply.github.com>
2024-01-13 11:13:49 +0100

    updated ports

--- controllers/register.js
+++ controllers/register.js
@@ -1,45 +1,55 @@
 const handleRegister = (req, res, bcrypt, db) => {
-    const { email, name, password } = req.body;
 
-    console.log('Request body:', req.body); // Add this line to log the request body
-
-    if (!(email && name && password)) {
-        return res.status(400).json('incorrect form of submission');
+    // Input validation
+    if(!(req.body.email && req.body.name && req.body.password)) {
+      return res.status(400).json('Invalid input');
     }
-    const hash = bcrypt.hashSync(password);
-
-    db.transaction((trx) => {
-        trx
-            .insert({
-                hash: hash,
-                email: email,
-            })
-            .into('login')
-            .returning('email')
-            .then((loginEmail) => {
-                console.log('Login email:', loginEmail); // Add this line to log the login email
-                return trx('users')
-                    .returning('*')
-                    .insert({
-                        email: loginEmail[0].email,
-                        name: name,
-                        joined: new Date(),
-                    })
-                    .then((user) => {
-                        res.json(user[0]);
-                    });
-            })
-            .then(trx.commit)
-            .catch((err) => {
-                console.log('Transaction error:', err); // Add this line to log the transaction error
-                trx.rollback;
-            });
-    }).catch((err) => {
-        console.log('Registration error:', err); // Add this line to log the registration error
-        res.status(400).json('could not register user');
+  
+    const hash = bcrypt.hashSync(req.body.password);
+    
+    // Log input
+    console.log('Register Request:', req.body); 
+    
+    // Try login insert
+    db('login').insert({
+      hash: hash,
+      email: req.body.email  
+    })
+    .returning('email')
+    .then(loginEmail => {
+  
+      // Log db result 
+      console.log('Login email inserted:', loginEmail);
+  
+      // Try user insert
+      return db('users').insert({
+        email: loginEmail[0].email,
+        name: req.body.name,
+        joined: new Date()
+      }) 
+      .returning('*');
+  
+    })
+    .then(user => {
+      
+      // Log user 
+      console.log('User inserted:', user);
+  
+      // Success
+      res.json(user[0]);
+  
+    })
+    .catch(err => {
+  
+      // Log error
+      console.error('Registration error', err);
+      
+      res.status(400).json('Registration failed');
+  
     });
-};
-
-module.exports = {
-    handleRegister: handleRegister,
-};
+  
+  };
+  
+  module.exports = {
+    handleRegister
+  }
